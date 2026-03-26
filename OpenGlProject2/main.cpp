54105a8aaeeec16dd1d1f90b774e598702399934
Мария Кутрань <mariakutran@MacBook-Pro-Maria.local>
2026-03-26 18:54:19 +0300

    first commit

---
+++ OpenGlProject2/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  OpenGlProject2
+//
+//  Created by Мария Кутрань on 26.03.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+    
+    
+}
